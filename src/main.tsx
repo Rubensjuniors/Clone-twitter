11ffca5923f0890f743493b73aaf6aa2d083371c
rubens.marques <rubens.marques@hotmart.com>
2023-05-11 14:32:24 -0300

    Refactor: refeito o responsivo da timeLine e sidebar para fisrtMobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./Global.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
--- src/page/TimeLine/TimeLine.tsx
+++ src/page/TimeLine/TimeLine.tsx
@@ -1,4 +1,4 @@
-import "./_TimeLine.scss";
+import "./TimeLine.scss";
 import Header from "../../components/Header/Header";
 import Separator from "../../components/Separator/Separator";
 import Tweet from "../../components/Tweet/Tweet";
@@ -6,11 +6,11 @@ import { FormEvent, KeyboardEvent, useState } from "react";
 import { PaperPlaneRight } from "@phosphor-icons/react";
 
 const TimeLine = () => {
-  const [newTweet, setNewTweet] = useState('');
+  const [newTweet, setNewTweet] = useState("");
   const [tweet, setTweet] = useState([
-    'Meu primeiro tweet',
-    'Teste',
-    'Deu certo tweetar!'
+    "Meu primeiro tweet",
+    "Teste",
+    "Deu certo tweetar!",
   ]);
 
   const createNewTweet = (event: FormEvent) => {
@@ -22,34 +22,37 @@ const TimeLine = () => {
     setNewTweet("");
   };
 
-  const handleHotkeySubmit = ({key, ctrlKey, metaKey}: KeyboardEvent)=>{
-    if(key === 'Enter' && (ctrlKey || metaKey)){
-       setTweet([newTweet, ...tweet])
-      setNewTweet('')
+  const handleHotkeySubmit = ({ key, ctrlKey, metaKey }: KeyboardEvent) => {
+    if (key === "Enter" && (ctrlKey || metaKey)) {
+      setTweet([newTweet, ...tweet]);
+      setNewTweet("");
     }
- }
+  };
 
   return (
     <>
-      <main className="timeline">
+      <main>
         <Header title="Home" />
 
-        <form onSubmit={createNewTweet} className="newTweet__form">
-          <label htmlFor="Tweet">
-            <img
-              src="https://avatars.githubusercontent.com/u/104171589?v=4"
-              alt="Rubensjunio"
-            />
+        <form onSubmit={createNewTweet} className="timeline-form">
+          <label htmlFor="Tweet" className="timeline-form__label">
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/104171589?v=4"
+                alt="Rubensjunio"
+              />
+            </div>
             <textarea
               name="Tweet"
               id="Tweet"
               value={newTweet}
               placeholder="What is thinking?"
               onKeyDown={handleHotkeySubmit}
-              onChange={event => setNewTweet(event.target.value)}
+              onChange={(event) => setNewTweet(event.target.value)}
             ></textarea>
           </label>
-          <button type="submit">
+
+          <button className="timeline-form__button" type="submit">
             <span>Tweet</span>
             <PaperPlaneRight />
           </button>
